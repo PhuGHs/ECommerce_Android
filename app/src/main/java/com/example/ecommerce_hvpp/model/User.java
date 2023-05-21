8061e7b0fa4f8f10a2d1087768f70e597ac760ad
hoangphuc38 <hoangxuanhanductho@gmail.com>
2023-05-22 00:12:25 +0700

    Set text for Edit_Profile

--- app/src/main/java/com/example/ecommerce_hvpp/model/User.java
+++ app/src/main/java/com/example/ecommerce_hvpp/model/User.java
@@ -17,11 +17,13 @@ public class User {
     private boolean isAdmin;
     private String username = "";
 
-    public User(boolean isAdmin, String username, String email, String password, String imagePath) {
+    public User(boolean isAdmin, String username, String email, String password, String datebirth, String address, String imagePath) {
         this.isAdmin = isAdmin;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.datebirth = datebirth;
+        this.address = address;
         this.imagePath = imagePath;
     }
 
@@ -40,6 +42,16 @@ public class User {
 
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
+    }
+    private String address = "";
+
+    public String getAddress() {
+        return address;
+    }
+    public String datebirth = "";
+
+    public String getDatebirth() {
+        return datebirth;
     }
 
     public String getImagePath() {
--- app/src/main/java/com/example/ecommerce_hvpp/viewmodel/ProfileViewModel.java
+++ app/src/main/java/com/example/ecommerce_hvpp/viewmodel/ProfileViewModel.java
@@ -2,25 +2,18 @@ package com.example.ecommerce_hvpp.viewmodel;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.ecommerce_hvpp.model.User;
-import com.example.ecommerce_hvpp.model.UserInfo;
 import com.example.ecommerce_hvpp.repositories.ProfileRepository;
 import com.example.ecommerce_hvpp.util.Resource;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileViewModel extends ViewModel {
     private ProfileRepository repo = new ProfileRepository();
     private FirebaseAuth firebaseAuth;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private String name;
-    private MutableLiveData<Resource<UserInfo>> _userInfo = new MutableLiveData<>();
     public LiveData<Resource<User>> showUserName(){
         FirebaseUser fbUser = firebaseAuth.getInstance().getCurrentUser();
         Log.e("Phuc", fbUser.getEmail());
