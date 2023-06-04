883a7159c71c666ed700e8733c981e6903bce6d4
Phu Le <21522466@gm.uit.edu.vn>
2023-06-04 15:51:03 +0700

    User Authorization: finish

--- app/src/main/java/com/example/ecommerce_hvpp/activities/MainActivity.java
+++ app/src/main/java/com/example/ecommerce_hvpp/activities/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.ecommerce_hvpp.R;
+import com.example.ecommerce_hvpp.firebase.FirebaseHelper;
+import com.example.ecommerce_hvpp.repositories.customerRepositories.UserRepository;
 import com.example.ecommerce_hvpp.viewmodel.Customer.ProductViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,13 +18,30 @@ public class MainActivity extends AppCompatActivity {
     public static ProductViewModel PDviewModel;
     private BottomNavigationView bottomNav;
     private NavController navController;
+    private FirebaseHelper fbHelper;
+    private UserRepository repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fbHelper = FirebaseHelper.getInstance();
         setContentView(R.layout.activity_main);
+
 
         bottomNav = findViewById(R.id.bottom_nav);
         navController = Navigation.findNavController(this, R.id.host_fragment);
+
+        String Id = fbHelper.getAuth().getCurrentUser().getUid();
+        fbHelper.getCollection("users").document(Id).get()
+                        .addOnSuccessListener(documentSnapshot -> {
+                            boolean isAdmin = documentSnapshot.getBoolean("admin");
+                            if(isAdmin) {
+                                bottomNav.inflateMenu(R.menu.admin_bottom_menu);
+                                navController.setGraph(R.navigation.admin_nav_graph);
+                            } else {
+                                bottomNav.inflateMenu(R.menu.bottom_menu);
+                                navController.setGraph(R.navigation.nav_graph);
+                            }
+                        });
 
         NavigationUI.setupWithNavController(bottomNav, navController);
 
