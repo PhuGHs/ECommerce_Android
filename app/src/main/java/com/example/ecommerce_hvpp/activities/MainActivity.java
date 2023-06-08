648ab9ce8158c451845cc5c3d0c2cea6ff18645c
Phu Le <21522466@gm.uit.edu.vn>
2023-06-08 09:47:45 +0700

    add internet broadcast to handle network connection change

--- app/src/main/java/com/example/ecommerce_hvpp/activities/MainActivity.java
+++ app/src/main/java/com/example/ecommerce_hvpp/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ecommerce_hvpp.activities;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,47 +14,84 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.ecommerce_hvpp.R;
 import com.example.ecommerce_hvpp.firebase.FirebaseHelper;
 import com.example.ecommerce_hvpp.repositories.customerRepositories.UserRepository;
+import com.example.ecommerce_hvpp.util.NetworkChangeBroadcastReceiver;
 import com.example.ecommerce_hvpp.viewmodel.Customer.ProductViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NetworkChangeBroadcastReceiver.NetworkConnectivityListener {
     public static ProductViewModel PDviewModel;
     private BottomNavigationView bottomNav;
     private NavController navController;
     private FirebaseHelper fbHelper;
     private UserRepository repo;
+
+    private NetworkChangeBroadcastReceiver networkChangeBroadcastReceiver;
+    private RelativeLayout noInternetLayout, hasInternetLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fbHelper = FirebaseHelper.getInstance();
         setContentView(R.layout.activity_main);
 
+        noInternetLayout = findViewById(R.id.noInternetLayout);
+        hasInternetLayout = findViewById(R.id.hasInternetLayout);
+
+        networkChangeBroadcastReceiver = new NetworkChangeBroadcastReceiver();
+        networkChangeBroadcastReceiver.setListener(this);
 
         bottomNav = findViewById(R.id.bottom_nav);
         navController = Navigation.findNavController(this, R.id.host_fragment);
 
         String Id = fbHelper.getAuth().getCurrentUser().getUid();
         fbHelper.getCollection("users").document(Id).get()
-                        .addOnSuccessListener(documentSnapshot -> {
-                            boolean isAdmin = documentSnapshot.getBoolean("admin");
-                            if(isAdmin) {
-                                bottomNav.inflateMenu(R.menu.admin_bottom_menu);
-                                navController.setGraph(R.navigation.admin_nav_graph);
-                            } else {
-                                bottomNav.inflateMenu(R.menu.bottom_menu);
-                                navController.setGraph(R.navigation.nav_graph);
-                            }
-                        });
+                .addOnSuccessListener(documentSnapshot -> {
+                    boolean isAdmin = documentSnapshot.getBoolean("admin");
+                    if(isAdmin) {
+                        bottomNav.inflateMenu(R.menu.admin_bottom_menu);
+                        navController.setGraph(R.navigation.admin_nav_graph);
+                    } else {
+                        bottomNav.inflateMenu(R.menu.bottom_menu);
+                        navController.setGraph(R.navigation.nav_graph);
+                    }
+                });
 
         NavigationUI.setupWithNavController(bottomNav, navController);
 
         PDviewModel.initData();
     }
 
+    public void showMainLayout() {
+        hasInternetLayout.setVisibility(View.VISIBLE);
+        noInternetLayout.setVisibility(View.GONE);
+        bottomNav.setVisibility(View.VISIBLE);
+    }
+
+    public void showNoNetworkLayout() {
+        noInternetLayout.setVisibility(View.VISIBLE);
+        hasInternetLayout.setVisibility(View.GONE);
+        bottomNav.setVisibility(View.GONE);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
+        registerReceiver(networkChangeBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(networkChangeBroadcastReceiver);
+    }
+
+    @Override
+    public void onNetworkConnectivityChanged(boolean isConnected) {
+        if(isConnected) {
+            showMainLayout();
+        } else {
+            showNoNetworkLayout();
+        }
     }
 }
