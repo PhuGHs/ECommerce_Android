9200632bfc48c180e3f34c9c17101a488e9d362f
AmosIvor <trantuanvu00000@gmail.com>
2023-06-15 14:31:29 +0700

    some changes in RegisterLoginActivity.java

--- app/src/main/java/com/example/ecommerce_hvpp/activities/RegisterLoginActivity.java
+++ app/src/main/java/com/example/ecommerce_hvpp/activities/RegisterLoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 
